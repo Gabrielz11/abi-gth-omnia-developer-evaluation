e181751422a2ecc628ea2b819dd4caf2ec077479
Gabriel Aires <gvaires@gmail.com>
2025-05-05 10:38:07 -0300

    feat: include the entities sale and saleitem

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/SaleItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class SaleItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public bool IsCancelled { get; set; }
+    }
+}
